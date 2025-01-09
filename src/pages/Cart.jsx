1462edecdf56e5afbbfab908bff81fc04f64a11c
Juan Pablo Figueroa <juanpablodejesusfigueroa@gmail.com>
2025-01-09 15:47:12 -0600

    Terminando el carrito de compras

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,9 +1,12 @@
 import { useContext, useEffect, useState } from "react";
 import { ShopContext } from "../context/ShopContext";
 import Title from "../components/Title/Title";
+import { assets } from "../assets/assets";
+import CartTotal from "../components/CartTotal/CartTotal";
 
 const Cart = () => {
-  const { products, currency, cartItems } = useContext(ShopContext);
+  const { products, currency, cartItems, updateQuantity, navigate } =
+    useContext(ShopContext);
   const [cartData, setCartData] = useState([]);
   useEffect(() => {
     const tempData = [];
@@ -12,7 +15,7 @@ const Cart = () => {
         if (cartItems[items][item] > 0) {
           tempData.push({
             _id: items,
-            size: items,
+            size: item,
             quantity: cartItems[items][item],
           });
         }
@@ -47,11 +50,53 @@ const Cart = () => {
                     <p className="text-xs sm:text-lg font-medium">
                       {productData.name}
                     </p>
+                    <div className="flex items-center gap-5 mt-2">
+                      <p>
+                        {currency}
+                        {productData.price}
+                      </p>
+                      <p className="px-2 sm:px-3 sm:py-1 border bg-slate-50">
+                        {item.size}
+                      </p>
+                    </div>
                   </div>
                 </div>
+                <input
+                  type="number"
+                  onChange={(e) =>
+                    e.target.value === "" || e.target.value === "0"
+                      ? null
+                      : updateQuantity(
+                          item._id,
+                          item.size,
+                          Number(e.target.value)
+                        )
+                  }
+                  className="border max-w-10 sm:max-w-20 px-1 sm:px-2 py-1"
+                  min={1}
+                  defaultValue={item.quantity}
+                />
+                <img
+                  src={assets.bin_icon}
+                  onClick={() => updateQuantity(item._id, item.size, 0)}
+                  className="w-4 mr-4 sm:w-5 cursor-pointer"
+                />
               </div>
             );
           })}
+        </div>
+        <div className="flex justify-end my-20">
+          <div className="w-full sm:w-[450px]">
+            <CartTotal />
+            <div className="w-full text-end">
+              <button
+                onClick={() => navigate("/place-order")}
+                className="bg-black text-white text-sm my-8 px-8 py-3"
+              >
+                PROCEED TO CHECKOUT
+              </button>
+            </div>
+          </div>
         </div>
       </div>
     </>
