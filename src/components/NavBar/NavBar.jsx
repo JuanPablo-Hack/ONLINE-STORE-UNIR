8e13544146ad9a4a2c8a6d54335f54c486e4e223
Juan Pablo Figueroa <juanpablodejesusfigueroa@gmail.com>
2025-01-09 16:27:22 -0600

    Acabando la plantilla

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -38,20 +38,6 @@ const NavBar = () => {
           alt="Search"
         />
         {/* TODO: Realizar el BEM de este apartado en CSS */}
-        <div className="group relative">
-          <img
-            src={assets.profile_icon}
-            className="w-5 cursor-pointer"
-            alt="Profile"
-          />
-          <div className="group-hover:block hidden absolute dropdown_menu right-0 pt-4">
-            <div className="flex flex-col gap-2 w-36 py-3 px-5 bg-slate-100 text-gray-500 rounded">
-              <p className="cursor-pointer hover:text-black">My profile</p>
-              <p className="cursor-pointer hover:text-black">Orders</p>
-              <p className="cursor-pointer hover:text-black">Logout</p>
-            </div>
-          </div>
-        </div>
         <Link to="/cart" className="relative">
           <img src={assets.cart_icon} className="w-5 min-w-5" alt="Cart" />
           <p className="absolute right-[-5px] bottom-[-5px] w-4 text-center leading-4 bg-black text-white aspect-square rounded-full text-[8px]">
