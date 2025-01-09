24892bccbfca47490a5c05e275c0a267f22777de
Juan Pablo Figueroa <juanpablodejesusfigueroa@gmail.com>
2025-01-09 16:31:10 -0600

    terminando parte funcional

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import About from "./pages/About";
 import Cart from "./pages/Cart";
 import Collections from "./pages/Collections";
 import Contact from "./pages/Contact";
+import Orders from "./pages/Orders";
 import PlaceOrder from "./pages/PlaceOrder";
 import Product from "./pages/Product";
 import NavBar from "./components/NavBar/NavBar";
@@ -24,6 +25,7 @@ function App() {
         <Route path="/cart" element={<Cart />} />
         <Route path="/collections" element={<Collections />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/orders" element={<Orders />} />
         <Route path="/place-order" element={<PlaceOrder />} />
         <Route path="/product/:id" element={<Product />} />
       </Routes>
