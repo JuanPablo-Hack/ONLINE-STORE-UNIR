1eb6582e6cf603fbd39c12cb43a5cf281041119a
Juan Pablo Figueroa <juanpablodejesusfigueroa@gmail.com>
2025-01-10 09:33:55 -0600

    Optimizando css contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -5,21 +5,17 @@ import Newsletter from "../components/Newsletter/Newsletter";
 const Contact = () => {
   return (
     <>
-      <div className="text-center text-2xl pt-10 border-t">
+      <div className="contacttitle">
         <Title text1={"CONTACT"} text2={"US"} />
       </div>
-      <div className="my-10 flex flex-col justify-center md:flex-row gap-10 mb-28">
-        <img
-          className="w-full md:max-w-[480px]"
-          src={assets.contact_img}
-          alt=""
-        />
-        <div className="flex flex-col justify-center items-start gap-6">
-          <p className="font-semibold text-xl text-gray-600">Our store</p>
-          <p className="text-gray-500">
+      <div className="contact">
+        <img className="contact__image" src={assets.contact_img} alt="" />
+        <div className="contact__info">
+          <p className="info__title">Our store</p>
+          <p className="info__text">
             28879 Villaurrutia <br /> Palmares del Pacifico{" "}
           </p>
-          <p className="text-gray-500">
+          <p className="info__text">
             Tel: (314)-120-4575 <br />
             Email: contact@foreveryou.com
           </p>
